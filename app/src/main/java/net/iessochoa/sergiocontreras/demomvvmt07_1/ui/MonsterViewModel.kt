67dec58d902c6074964ae700866703be2b2fb178
scontreraslopez <s.contreraslopez@edu.gva.es>
2025-11-04 10:05:27 +0100

    refactor: wip MonsterViewModel ready

--- app/src/main/java/net/iessochoa/sergiocontreras/demomvvmt07_1/ui/MonsterViewModel.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/demomvvmt07_1/ui/MonsterViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import net.iessochoa.sergiocontreras.demomvvmt07_1.data.MonsterRepository
 
 /**
  * Project: DemoMVVMT07-1
@@ -17,7 +19,37 @@ class MonsterViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(MonsterUiState())
     val uiState: StateFlow<MonsterUiState> = _uiState.asStateFlow()
 
+    fun onAttack(minDamage: Int = 1, maxDamage: Int = 3) {
+        //1. Obtener el estado actual
+        val currentState = _uiState.value
+
+        // Lógica de daño
+        val damage = (minDamage..maxDamage).random()
+        val newHp = (currentState.currentHp - damage).coerceAtLeast(0) //coerceAtLeast es para forzar 0+
 
 
-
+        if (newHp == 0) {
+            // Lógica de "monstruo derrotado"
+            // Pillo un nuevo monstruo y bodycount++
+            val newMonster = MonsterRepository.getNextMonster()
+            _uiState.update {
+                it.copy(
+                    name = newMonster.name,
+                    currentHp = newMonster.maxHp,
+                    maxHp = newMonster.maxHp,
+                    spriteId = newMonster.spriteId,
+                    bodyCount = currentState.bodyCount + 1
+                )
+            }
+        } else {
+            // Lógica de "sigue vivo", solo tengo que actualizar el hp
+            _uiState.update {
+                it.copy(
+                    currentHp = newHp
+                    )
+                }
+            }
+        }
 }
+
+
