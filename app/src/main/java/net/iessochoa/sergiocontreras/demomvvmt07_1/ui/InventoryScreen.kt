3206bbb3ec99214d5bb89732e0cf9c7a0906ea91
scontreraslopez <s.contreraslopez@edu.gva.es>
2025-11-04 10:46:15 +0100

    feat: now with gold image and proper style

---
+++ app/src/main/java/net/iessochoa/sergiocontreras/demomvvmt07_1/ui/InventoryScreen.kt
@@ -0,0 +1,62 @@
+package net.iessochoa.sergiocontreras.demomvvmt07_1.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import net.iessochoa.sergiocontreras.demomvvmt07_1.R
+import net.iessochoa.sergiocontreras.demomvvmt07_1.ui.theme.Typography
+
+/**
+ * Project: DemoMVVMT07-1
+ * From: net.iessochoa.sergiocontreras.demomvvmt07_1.ui
+ * Created by: Contr
+ * On: 04/11/2025 at 10:26
+ * Creado en Settings -> Editor -> File and Code Templates
+ */
+
+/** Esta es para trabajar la navegación
+ * la idea es que en MainScreen tengas un botón con un cofre
+ * al hacerle click navegas a tu inventario
+ * tu inventario de momento solo mostrará gold: $bodycount
+ * abajo una imagen de monedas de oro, si y solo si gold > 0
+ */
+@Composable
+fun InventoryScreen(bodycount: Int) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "gold: $bodycount",
+            style = Typography.headlineLarge,
+            fontWeight = FontWeight.Bold
+        )
+        if (bodycount > 0) {
+            Image(
+                painter = painterResource(R.drawable.gold),
+                contentDescription = "Gold"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InventoryScreenPreviewWithGold() {
+    InventoryScreen(bodycount = 100)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InventoryScreenPreviewWithoutGold() {
+    InventoryScreen(bodycount = 0)
+}
